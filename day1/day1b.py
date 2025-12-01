b6d62937af19d49a57ca063a0e5269e16f64384c
mshalvagal <mshalvagal@gmail.com>
2025-12-01 00:49:05 -0500

    day 1

---
+++ day1/day1b.py
@@ -0,0 +1,70 @@
+import re
+import typing
+import math
+from collections.abc import Iterable
+import sys
+import numpy as np
+
+def lmap(func, *iterables):
+    return list(map(func, *iterables))
+def ints(s: str) -> typing.List[int]:
+    return lmap(int, re.findall(r"-?\d+", s))
+def positive_ints(s: str) -> typing.List[int]:
+    return lmap(int, re.findall(r"\d+", s))
+def floats(s: str) -> typing.List[float]:
+    return lmap(float, re.findall(r"-?\d+(?:\.\d+)?", s))
+def positive_floats(s: str) -> typing.List[float]:
+    return lmap(float, re.findall(r"\d+(?:\.\d+)?", s))
+def words(s: str) -> typing.List[str]:
+    return re.findall(r"[a-zA-Z]+", s)
+def alphanums(s: str) -> typing.List[str]:
+    return re.findall(r"[a-zA-Z0-9]+", s)
+def lfilter(func, *iterables):
+    return list(filter(func, *iterables))
+def pprint(a, name):
+    print(f'\n{name}:\n{a}')
+
+import random
+from collections import defaultdict
+
+def solve(path):
+    ans = 0
+    with open(path) as f:
+        lines = f.read().strip().split('\n')
+    
+    pos = 50
+    count = 0
+    for line in lines:
+        dir = line[0]
+        dist = int(line[1:])
+        if dir == 'L':
+            dist = -dist
+        start_pos = pos
+        pos += dist
+        new_pos = pos % 100
+        if new_pos != pos:
+            count += abs(pos) // 100
+            if pos < 0 and start_pos != 0:
+                count += 1
+            if pos == 0:
+                count += 1
+        else:
+            if new_pos == 0:
+                count += 1
+        # print(pos, new_pos, count)
+        pos = new_pos
+
+    ans = count
+
+    return int(ans)
+
+EXAMPLE_ANS = 6
+
+ex_ans = solve('./example1.txt')
+
+if ex_ans == EXAMPLE_ANS:
+    print("\nWorks on the example! Trying full case.\n")
+    final_ans = solve('./input1.txt')
+    print("=====>     ", final_ans, "     <=====\n")
+else:
+    print(f"\nDoes not work on the example, got {ex_ans} instead of {EXAMPLE_ANS}\n")
